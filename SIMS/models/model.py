25f99a96f63b6184c47c69e49070dcd9306b1b27
123sprouting <153815695+123sprouting@users.noreply.github.com>
2024-12-13 15:16:59 +0800

    Update model.py

--- SIMS/models/model.py
+++ SIMS/models/model.py
@@ -132,12 +132,7 @@ class TVA_fusion(nn.Module):
             loss_a = self.get_KL_loss(x_a_embed, x_a_embed_froze)  
             loss_nce = self.get_InfoNCE_loss(x_v_embed, x_t_embed) + self.get_InfoNCE_loss(x_a_embed, x_t_embed) 
         else:
-            return pred, (x_v_embed,x_a_embed)
-            # model_tva = self.TVA_decoder.MLP[2:]
-            # pred_tva = model_tva(x_tva)
-            # return pred, pred_tva
-            
-            
+            return pred, (x_v_embed,x_a_embed)  
         return pred, (loss_v, loss_a, loss_nce)
         
     def save_model(self,name=None):
